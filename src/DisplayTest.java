949147f73a797a72caa18f040762865e7e943dcc
Evan Moelter <evanmoelter@umail.ucsb.edu>
2013-10-13 22:36:12 -0700

    added FPS counter

--- src/DisplayTest.java
+++ src/DisplayTest.java
@@ -3,6 +3,14 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 
 public class DisplayTest {
+
+	/** time at last frame */
+	long lastFrame;
+	/** frames per second */
+	int fps;
+	/** last fps time */
+	long lastFPS;
+
 	public void start() {
 		try {
 			Display.setDisplayMode(new DisplayMode(800,600));
@@ -14,12 +22,27 @@ public class DisplayTest {
 		}
 		while (!Display.isCloseRequested()) {
 			Display.update();
-		}		
+			Display.sync(60);
+		}
 		Display.destroy();
 	}
-	
+
 	public static void main(String[] argv) {
 		DisplayTest displayExample = new DisplayTest();
 		displayExample.start();
+	}
+	public void getTime()
+	{
+		return Sys.getTime() * 1000 / Sys.getTimeResolution();
+	}
+
+	public void updateFPS()
+	{
+		if (getTime() - lastFPS > 1000) {
+			Display.setTitle("FPS: " + fps);
+			fps = 0;
+			lastFPS += 1000;
+		}
+		fps++;
 	}
 }
