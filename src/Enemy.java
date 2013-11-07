8e4546d71233349544e7334fa66f5a29bf179c13
James Thompson <jsthompson@umail.ucsb.edu>
2013-11-07 14:32:07 -0800

    Added Enemy class

---
+++ src/Enemy.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: James Thompson
+ * Date: 11/7/13
+ * Time: 2:30 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public abstract class Enemy {
+}
