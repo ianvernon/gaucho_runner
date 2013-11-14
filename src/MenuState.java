7682c1b78b43623f1a2614cd04c364fe7ce96678
James Thompson <jsthompson@umail.ucsb.edu>
2013-11-13 17:28:43 -0800

    Sound works again and moved sound files to a folder

--- src/MenuState.java
+++ src/MenuState.java
@@ -50,6 +50,8 @@ public class MenuState extends BasicGameState {
     private Image quitS;
     /** GUI context of menu state */
     private GUIContext guiContext;
+    public SoundManager soundManager;
+
 
     /**
      * Initializes Menu state
@@ -141,6 +143,8 @@ public class MenuState extends BasicGameState {
 
             bg = new Image(bgPath);
             initMenu(gc, sbg);
+            soundManager = new SoundManager();
+            soundManager.play();
         } catch (SlickException ex) {
             ex.printStackTrace();
             return;
--- src/SoundManager.java
+++ src/SoundManager.java
@@ -4,6 +4,7 @@ import org.newdawn.slick.openal.AudioLoader;
 import org.newdawn.slick.util.ResourceLoader;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * A class that loads a sound resource and makes it available for output
@@ -15,25 +16,31 @@ public class SoundManager {
     private Audio oggStream;
     /** WAV file stream */
     private Audio wav;
+    private ArrayList<String> srcList;
+    public ArrayList<Audio> soundList;
+
 
     /**
      * Initializes the source of sound
-     * @param src
      */
-    public SoundManager(String src) {
-        this.src = src;
+    public SoundManager() {
+        srcList = new ArrayList<String>();
+        srcList.add("res/sound/Testarossa.wav");
+        srcList.add("res/sound/theme.wav");
+        init();
     }
 
     /**
      * Loads sound resource
      */
     public void init() {
-        try {
-//			oggStream = AudioLoader.getStreamingAudio("OGG", ResourceLoader.getResource(src));
-            wav = AudioLoader.getAudio("WAV", ResourceLoader.getResourceAsStream(src));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            try {
+                wav = (AudioLoader.getAudio("WAV", ResourceLoader.getResourceAsStream(srcList.get(0))));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
     }
 
     /**
@@ -47,8 +54,9 @@ public class SoundManager {
      * Plays sound resource
      */
     public void play() {
-//		oggStream.playAsMusic(1.0f, 1.0f, true);
         wav.playAsMusic(1.0f, 1.0f, true);
+//         soundList.get(0).playAsMusic(1.0f, 1.0f, true);
+
     }
 
 }
